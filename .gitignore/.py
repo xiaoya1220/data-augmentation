aeaae02f34076d5d68c79ea2120fce2d353f928b
Adamancy小亚 <30307508+xiaoya1220@users.noreply.github.com>
2018-01-09 17:30:18 +0800

    Update and rename .gitignore to .py

---
+++ .gitignore/.py
@@ -0,0 +1,52 @@
+import tensorflow as tf
+import matplotlib.image as mpimg
+import numpy as np
+
+IMAGE_SIZE = 224
+
+def tf_resize_images(X_img_file_paths):
+    X_data = []
+    tf.reset_default_graph()
+    X = tf.placeholder(tf.float32, (None, None, 3))
+    tf_img = tf.image.resize_images(X, (IMAGE_SIZE, IMAGE_SIZE), 
+                                    tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        
+        # Each image is resized individually as different image may be of different size.
+        for index, file_path in enumerate(X_img_file_paths):
+            img = mpimg.imread(file_path)[:, :, :3] # Do not read alpha channel.
+            resized_img = sess.run(tf_img, feed_dict = {X: img})
+            X_data.append(resized_img)
+
+    X_data = np.array(X_data, dtype = np.float32) # Convert to numpy
+    return X_data
+    
+    def central_scale_images(X_imgs, scales):
+    # Various settings needed for Tensorflow operation
+    boxes = np.zeros((len(scales), 4), dtype = np.float32)
+    for index, scale in enumerate(scales):
+        x1 = y1 = 0.5 - 0.5 * scale # To scale centrally
+        x2 = y2 = 0.5 + 0.5 * scale
+        boxes[index] = np.array([y1, x1, y2, x2], dtype = np.float32)
+    box_ind = np.zeros((len(scales)), dtype = np.int32)
+    crop_size = np.array([IMAGE_SIZE, IMAGE_SIZE], dtype = np.int32)
+    
+    X_scale_data = []
+    tf.reset_default_graph()
+    X = tf.placeholder(tf.float32, shape = (1, IMAGE_SIZE, IMAGE_SIZE, 3))
+    # Define Tensorflow operation for all scales but only one base image at a time
+    tf_img = tf.image.crop_and_resize(X, boxes, box_ind, crop_size)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        
+        for img_data in X_imgs:
+            batch_img = np.expand_dims(img_data, axis = 0)
+            scaled_imgs = sess.run(tf_img, feed_dict = {X: batch_img})
+            X_scale_data.extend(scaled_imgs)
+    
+    X_scale_data = np.array(X_scale_data, dtype = np.float32)
+    return X_scale_data
+	
+# Produce each image at scaling of 90%, 75% and 60% of original image.
+scaled_imgs = central_scale_images(X_imgs, [0.90, 0.75, 0.60])
